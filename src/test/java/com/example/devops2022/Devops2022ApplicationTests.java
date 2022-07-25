b51e5c2d3d9044959f78c25e3a5e1bba6325f671
unknown <anshulgoel48@yahoo.com.au>
2022-07-25 14:40:20 +0530

    initial

---
+++ src/test/java/com/example/devops2022/Devops2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devops2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devops2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
